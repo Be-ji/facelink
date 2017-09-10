ab4db38ad2780bad40756ae00f2ec78751c492d1
Bejan Qotbi <be-ji@hotmail.com>
2017-09-10 22:28:34 +0200

    Dimanche soir

--- FaceLink/src/main/java/be/bt/controller/UtilisateurController.java
+++ FaceLink/src/main/java/be/bt/controller/UtilisateurController.java
@@ -3,11 +3,17 @@ package be.bt.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import be.bt.entity.Utilisateur;
 import be.bt.service.UtilisateurService;
@@ -34,6 +40,17 @@ public class UtilisateurController {
 	@GetMapping(path="/user/{id}")
 	public ResponseEntity <Utilisateur> get(@PathVariable Long id) {
 		return new ResponseEntity <>(service.trouverUtilisateur(id), HttpStatus.OK);
+	}
+	
+	//@RequestMapping(path="/signup", method=RequestMethod.POST, headers="Content-Type=application/json")
+	@PostMapping("signup")
+	public ResponseEntity<Void> add(@RequestBody Utilisateur utilisateur, UriComponentsBuilder builder) {
+
+		service.ajouterUtilisateur(utilisateur);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(builder.path("/user/{id}").buildAndExpand(utilisateur.getId()).toUri());
+		return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
 	}
 	
 }
